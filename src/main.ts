7693d0573960d0faf3ab96d2025d0674d4d0a880
pulidoepee <diegojesuspulido@gmail.com>
2026-09-20 15:32:22 +0100

    Configurar CORS para produccion

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://fundacionrubenlimardogascon.org',
+      'https://www.fundacionrubenlimardogascon.org',
+    ],
     methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
     credentials: true,
   });
